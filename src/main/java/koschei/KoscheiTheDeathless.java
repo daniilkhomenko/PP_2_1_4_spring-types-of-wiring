318c54a46b32a8102999446eb2333446371abb69
daniilkhomenko <daniil.khomenko@yahoo.com>
2022-05-16 16:18:52 +0300

    updated

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,6 +1,6 @@
 package koschei;
 
-import koschei.models.Ocean1;
+import koschei.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
